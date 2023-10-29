68865e79e817a3f76890c22114a5e174aaf6ed01
jehad <jehadmukahal1@gmail.com>
2023-10-29 18:23:28 +0300

    enhancing the gui app

--- gui.py
+++ gui.py
@@ -1,9 +1,28 @@
 import PySimpleGUI as gui
-import functions
+import functions as f
 
 label = gui.Text("type in a name: ")
-inputBox = gui.InputText("enter a name")
+inputBox = gui.InputText(tooltip = "enter a name", key ='name')
 button1 = gui.Button("add")
-window = gui.Window("Name Program", layout=[[label], [inputBox, button1]])
-window.read()
+window = gui.Window("Name Program", 
+                    layout=[[label], [inputBox, button1]],
+                    font = ('helvetica', 15))
+
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    if 'add':
+        mylist = f.getNames()
+        name = values["name"] + "\n"
+        mylist.append(name)
+        f.storeNames(mylist)
+    if gui.WINDOW_CLOSED:
+        break
+        
+        
+        
+    
+    
 window.close()
