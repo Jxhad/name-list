5ad6c027c2e5c05e775586c1c7fed9a7ec3e84f8
jehad <jehadmukahal1@gmail.com>
2023-10-29 20:54:14 +0300

    adding a edit feature

--- gui.py
+++ gui.py
@@ -2,27 +2,37 @@ import PySimpleGUI as gui
 import functions as f
 
 label = gui.Text("type in a name: ")
-inputBox = gui.InputText(tooltip = "enter a name", key ='name')
-button1 = gui.Button("add")
-window = gui.Window("Name Program", 
-                    layout=[[label], [inputBox, button1]],
-                    font = ('helvetica', 15))
+inputBox = gui.InputText(tooltip="enter a name", key='name')
+addButton = gui.Button("add")
+editButton = gui.Button("edit")
+listBox = gui.Listbox(f.getNames(), key="names", enable_events=True, size=[40, 10])
 
+window = gui.Window("Name Program",
+                    layout=[[label], [inputBox, addButton], [listBox, editButton]],
+                    font=('helvetica', 15))
 
 while True:
     event, values = window.read()
     print(event)
     print(values)
-    if 'add':
-        mylist = f.getNames()
-        name = values["name"] + "\n"
-        mylist.append(name)
-        f.storeNames(mylist)
-    if gui.WINDOW_CLOSED:
-        break
+    match event:
+        case 'add':
+            mylist = f.getNames()
+            name = values["name"] + "\n"
+            mylist.append(name)
+            f.storeNames(mylist)
+            window['names'].update(values=mylist)
         
+        case 'edit':
+            nameEdit = values["names"][0]
+            newName = values['name']
+            mylist = f.getNames()
+            index = mylist.index(nameEdit)
+            mylist[index] = newName + "\n"
+            f.storeNames(mylist)
+            window['names'].update(values=mylist)
         
-        
-    
-    
+        case gui.WINDOW_CLOSED:
+            break
+
 window.close()
